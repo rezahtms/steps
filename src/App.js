e5aff90be49c279ed29ed75e6ee881432c35dcdb
rezahatampoor <rezahtms@gmail.com>
2024-01-28 12:40:16 +0330

    add handling events to btns

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ const messages = [
 
 export default function App() {
   const step = 1;
+  const previous = () => {
+    alert("Previous");
+  };
+
+  const next = () => {
+    alert("Next");
+  };
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -17,10 +25,16 @@ export default function App() {
         Step {step}: {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={previous}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={next}
+        >
           next
         </button>
       </div>
