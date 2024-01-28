9a900792497690f9842f39b60064222d37faeaf6
rezahatampoor <rezahtms@gmail.com>
2024-01-28 16:25:07 +0330

    create separate component and import in app.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,10 @@
-import { useState } from "react";
-
-const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
-];
+import Step from "./Step";
 
 export default function App() {
-  const [step, setStep] = useState(1);
-  const [isOpen, setIsOpen] = useState(true);
-  const previous = () => {
-    if (step > 1) setStep((current) => current - 1);
-  };
-
-  const next = () => {
-    if (step < 3) setStep((current) => current + 1);
-  };
-
   return (
-    <>
-      <button
-        className="close"
-        onClick={() => setIsOpen((current) => !current)}
-      >
-        &times;
-      </button>
-      {isOpen && (
-        <div className="steps">
-          <div className="numbers">
-            <div className={step >= 1 ? "active" : ""}>1</div>
-            <div className={step >= 2 ? "active" : ""}>2</div>
-            <div className={step >= 3 ? "active" : ""}>3</div>
-          </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
-          <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={previous}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={next}
-            >
-              next
-            </button>
-          </div>
-        </div>
-      )}
-    </>
+    <div>
+      <Step />
+      <Step />
+    </div>
   );
 }
